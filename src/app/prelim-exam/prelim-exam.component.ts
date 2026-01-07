b677eb6db66b43969bc99cd552215ded218f4aa9
Hamid Bacaraman <hbacaraman64866@liceo.edu.ph>
2026-01-07 12:39:34 +0800

    Preliminary Hands-On Exam - Responsive Grid Layout

---
+++ src/app/prelim-exam/prelim-exam.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-prelim-exam',
+  standalone: true,
+  templateUrl: './prelim-exam.component.html',
+  styleUrls: ['./prelim-exam.component.scss']
+})
+export class PrelimExamComponent {}
